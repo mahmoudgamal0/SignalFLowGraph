019f69955efc3898969f5cbc9fb55e07954f4955
mahmoudg0 <mahmoud123468.mg@gmail.com>
2018-04-15 15:55:42 +0200

    Initial commit

---
+++ src/Controller.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    private SFG sfg;
+
+    public Controller(){
+        this.sfg = new SFG();
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+}
